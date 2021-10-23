f9e1a5f5ba9dcc3381db522cc9047b40ac064b92
edrhat <eduardomchortolandia@gmail.com>
2021-10-23 12:27:56 -0300

    Melhorei a interface e coloquei comando para instalar o youtube-dl quando o programa inicia

--- conversor.py
+++ conversor.py
@@ -1,36 +1,39 @@
 import os
 import tkinter as tk
-
+os.system("pip install youtube-dl")
 class Tela:
 
     def __init__(self, master):
 
-        self.link = tk.Label(janela, text="Link do vídeo:")
-        self.link["font"] = ("Lucida Console", 14)
-        self.link.config(foreground="white", bg="black")
-        self.link.place(x=7, y=14)
+        cab = tk.PhotoImage(file="cab.png")
+        self.img = tk.Label(janela, image=cab)
+        self.img.cab = cab
+        self.img.config(bg="#696969")
+        self.img.place(x=50,y=10)
+
+        rodape = tk.PhotoImage(file="rodape.png")
+        self.img2 = tk.Label(janela, image=rodape)
+        self.img2.rodape = rodape
+        self.img2.config(bg="#696969")
+        self.img2.place(x=0,y=200)
+
 
         self.linkE = tk.Entry(janela)
         self.linkE["font"] = ("Arial", 17)
         self.linkE.config(foreground="#A52A2A")
-        self.linkE.place(x=170, y=10, width=300)
+        self.linkE.place(x=50, y=100, width=300)
 
         self.bt = tk.Button(janela, text="Baixar MP3")
         self.bt["font"] = ("Lucida Console", "13")
         self.bt.config(bg="red", foreground="white")
-        self.bt.place(x=260, y=48, width=120)
+        self.bt.place(x=200, y=142, width=120)
         self.bt.bind("<Button-1>", self.baixar)
 
-        #.bt3 = tk.Button(janela, text="Baixar MP4")
-        #self.bt3["font"] = ("Lucida Console", "13")
-        #self.bt3.config(bg="green", foreground="white")
-        #self.bt3.place(x=400, y=48, width=120)
-        #self.bt3.bind("<Button-1>", self.baixar2)
 
         self.bt2 = tk.Button(janela, text="Limpar")
         self.bt2["font"] = ("Lucida Console", "13")
         self.bt2.config(bg="black", foreground="red")
-        self.bt2.place(x=170, y=48, width=80)
+        self.bt2.place(x=80, y=142, width=80)
         self.bt2.bind("<Button-1>", self.limpar)
 
 
@@ -41,15 +44,8 @@ class Tela:
 
         lnk = self.linkE.get()
 
-        lk = ("youtube-dl --extract-audio --audio-format mp3 ")+(lnk)
-        os.system(lk)
-
-   # def baixar2(self, event):
-
-      #  lnk = self.linkE.get()
-
-      #  lk = ("youtube-dl  --excract-audio --audio-format mp4 ") + (lnk)
-      #  os.system(lk)
+        os.system("youtube-dl --extract-audio --audio-format mp3 {}".format(lnk))
+        
 
     def limpar(self, event):
 
@@ -60,8 +56,8 @@ class Tela:
 janela = tk.Tk()
 
 Tela(janela)
-janela.config(bg="black")
-janela.geometry("700x100")
+janela.config(bg="#696969")
+janela.geometry("400x230")
 janela.title("Conversor de vídeos do Youtube")
 janela.resizable(width=False, height=False)
 janela.mainloop()
